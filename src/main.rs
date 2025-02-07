268bd106ad54d0644c665173da2953d54ae1312c
Vipin <sainivipin1998@gmail.com>
2025-02-08 01:31:53 +0530

    health_check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+
+
+async fn greet(req:HttpRequest)-> impl Responder{
+    let name= req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!",&name)
+
 }
+
+async fn health_check(req:HttpRequest)->impl Responder{
+    HttpResponse::Ok()
+}
+
+
+
+#[tokio::main]
+
+async fn main() ->Result<(),std::io::Error>{
+    HttpServer::new(||{
+        App::new().route("/",web::get().to(greet))
+        .route("/health_check", web::get().to(health_check))
+        .route("/{name}", web::get().to(greet))
+    }).bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
+
+// #[cfg(test)]
+// mod tests{
+//     use crate::health_check;
+//     #[tokio::test]
+//     async fn health_check_succeeds(){
+//         let response=health_check().await;
+//         assert!(response.status().is_success())
+//     }
+// }
---
+++ tests/health_check.rs
@@ -0,0 +1,5 @@
+use zero2prod::main;
+#[test]
+fn dummy_test(){
+    main()
+}
